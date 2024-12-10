d52c11d1b79894c2ded9ecc1750827ab774f6ef4
bobmadereren <williamspurkeland@hotmail.com>
2024-12-10 20:35:01 +0100

    3.2

--- 3/code.js
+++ 3/code.js
@@ -4,8 +4,12 @@ import { fileURLToPath } from 'url';
 
 let input = fs.readFileSync(join(dirname(fileURLToPath(import.meta.url)), 'input.txt')).toString();
 
-let match = [...input.matchAll(/mul\((\d{1,3}),(\d{1,3})\)/g)];
-
 // Part 1
-let result = match.reduce((sum, [_, a, b]) => sum + a * b, 0); 
+let match = [...input.matchAll(/mul\((\d{1,3}),(\d{1,3})\)/g)];
+let result = match.reduce((sum, [_, a, b]) => sum + a * b, 0);
 console.log(result);
+
+// Part 2
+let match2 = [...input.matchAll(/do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)/g)];
+let result2 = match2.reduce((([sum, active], [op, a, b]) => /mul/.test(op) ? [sum + active * a * b, active] : [sum, /do\(\)/.test(op)]), [0, true]);
+console.log(result2);
