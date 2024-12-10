5c0a44f37cc866606009f1e5eba51eb625589428
bobmadereren <williamspurkeland@hotmail.com>
2024-12-11 00:46:46 +0100

    8-2

--- 8/code.js
+++ 8/code.js
@@ -8,7 +8,7 @@ let data = input.split(/\n/).filter(line => line).map(line => line.split(''));
 let size = data.length;
 console.assert(data.every(row => row.length == size), "Not square");
 
-// Part 1
+// Part 1 & 2
 let antennas = {};
 
 for (let y = 0; y < size; y++) {
@@ -22,6 +22,7 @@ for (let y = 0; y < size; y++) {
 }
 
 let antinodes = data.map(row => row.map(_ => false));
+let antinodes2 = data.map(row => row.map(_ => false));
 
 for (let as of Object.values(antennas)) {
     for (let i = 0; i < as.length; i++) {
@@ -29,13 +30,28 @@ for (let as of Object.values(antennas)) {
             let [y0, x0] = as[i];
             let [y1, x1] = as[j];
             let [dy, dx] = [y1 - y0, x1 - x0];
+            // Part 1
             for (let [x, y] of [
                 [y1 + dy, x1 + dx],
                 [y0 - dy, x0 - dx]
             ]) if (y < size && y >= 0 && x < size && x >= 0) antinodes[y][x] = true;
+            // Part 2
+            while(y1 < size && y1 >= 0 && x1 < size && x1 >= 0){
+                antinodes2[y1][x1] = true;
+                y1 += dy;
+                x1 += dx;
+            }
+            while(y0 < size && y0 >= 0 && x0 < size && x0 >= 0){
+                antinodes2[y0][x0] = true;
+                y0 -= dy;
+                x0 -= dx;
+            }
         }
     }
 }
 
 let result = antinodes.flat().filter(b => b).length;
 console.log(result);
+
+let result2 = antinodes2.flat().filter(b => b).length;
+console.log(result2);
