ebd8f6a50d54212c61147b9ed59da93659827445
bobmadereren <williamspurkeland@hotmail.com>
2024-12-10 18:28:05 +0100

    2-1

---
+++ 2/code.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+let input = fs.readFileSync(join(dirname(fileURLToPath(import.meta.url)), 'input.txt')).toString();
+
+let data = input.split(/\n/).filter(line => line).map(line => line.split(' '));
+
+// Part 1
+function safe(report) {
+    let d = 0;
+    for (let i = 1; i < report.length; i++) {
+        let newD = report[i] - report[i - 1];
+        if (Math.abs(newD) > 3 || Math.abs(newD) < 1 || Math.abs(Math.sign(newD) - Math.sign(d)) == 2) return false;
+        d = newD;
+    }
+    return true;
+}
+
+let result = data.filter(safe).length;
+console.log(result);
+
