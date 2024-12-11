9cf6f8134bb7d9f68acdf7b6674ca16973550cff
bobmadereren <williamspurkeland@hotmail.com>
2024-12-11 17:49:28 +0100

    11-2

--- 11/code.js
+++ 11/code.js
@@ -4,10 +4,23 @@ import { fileURLToPath } from 'url';
 
 let input = fs.readFileSync(join(dirname(fileURLToPath(import.meta.url)), 'input.txt')).toString();
 
-// Part 1
-let data = input.replace(/\n/, '').split(/ /);
+// Part 1 & 2
+let data = Object.fromEntries(input.replace(/\n/, '').split(/ /).map(n => [n, 1]));
 
-for (let i = 0; i < 25; i++)
-    data = data.flatMap(d => d == 0 ? 1 : d.length % 2 ? d * 2024 : [d.slice(0, d.length / 2), Number(d.slice(d.length / 2, d.length))]).map(String);
+for (let i = 0; i < 75; i++) { // For part 1: 75 -> 25 
+    let dataa = {};
+    let add = (n, q) => dataa[n] ? dataa[n] += q : dataa[n] = q;
 
-console.log(data.length);
+    for (let [n, q] of Object.entries(data)) {
+        if (n == 0) add(1, q);
+        else if (n.length % 2) add(n * 2024, q);
+        else {
+            add(n.slice(0, n.length / 2), q);
+            add(Number(n.slice(n.length / 2, n.length)), q);
+        }
+    }
+    data = dataa;
+}
+
+let result = Object.values(data).reduce((sum, q) => sum + q, 0);
+console.log(result);
