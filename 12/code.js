da8a7d60acb73ba9aa409d5a54e955b120640599
bobmadereren <williamspurkeland@hotmail.com>
2024-12-12 12:53:05 +0100

    12-2

--- 12/code.js
+++ 12/code.js
@@ -10,10 +10,11 @@ let data = input.split(/\n/).filter(line => line).map(line => line.split(''));
 let size = data.length;
 console.assert(data.every(row => row.length == size), "Non-Square");
 
-// Part 1
+// Part 1 & 2
 let marked = data.map(row => row.map(() => false));
 let area = [];
 let perimiter = [];
+let corners = [];
 
 function bfs(y, x) {
     if (marked[y][x]) return;
@@ -21,20 +22,31 @@ function bfs(y, x) {
     marked[y][x] = true;
     area[area.length - 1]++;
 
-    for (let [y1, x1] of [[y - 1, x], [y + 1, x], [y, x - 1], [y, x + 1]])
-        if (0 <= x1 && 0 <= y1 && x1 < size && y1 < size && data[y1][x1] == data[y][x])
-            bfs(y1, x1);
+    let dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+    let peek = (dy, dx) => 0 <= x + dx && 0 <= y + dy && x + dx < size && y + dy < size && data[y + dy][x + dx] == data[y][x];
+
+    for (let [dy, dx] of dirs)
+        if (peek(dy, dx))
+            bfs(y + dy, x + dx);
         else
             perimiter[perimiter.length - 1]++;
+
+    for (let [dy, dx] of dirs)
+        if (!peek(dy, dx) && !peek(-dx, dy) || peek(dy, dx) && peek(-dx, dy) && !peek(dy - dx, dy + dx))
+            corners[corners.length - 1]++;
 }
 
 for (let y = 0; y < size; y++)
     for (let x = 0; x < size; x++)
-        if(!marked[y][x]){
+        if (!marked[y][x]) {
             area.push(0);
             perimiter.push(0);
+            corners.push(0);
             bfs(y, x);
         }
 
 let result = area.reduce((sum, _, i) => sum + area[i] * perimiter[i], 0);
 console.log(result);
+
+let result2 = area.reduce((sum, _, i) => sum + area[i] * corners[i], 0);
+console.log(result2);
