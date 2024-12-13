96ff8a28cfa9aec02575b778324633e122f21b26
bobmadereren <williamspurkeland@hotmail.com>
2024-12-13 14:12:10 +0100

    13-2

--- 13/code.js
+++ 13/code.js
@@ -4,11 +4,17 @@ import { fileURLToPath } from 'url';
 
 let input = fs.readFileSync(join(dirname(fileURLToPath(import.meta.url)), 'input.txt')).toString();
 
-let match = input.matchAll(/Button A: X\+(\d*), Y\+(\d*)\nButton B: X\+(\d*), Y\+(\d*)\nPrize: X=(\d*), Y=(\d*)/g);
+let match = [...input.matchAll(/Button A: X\+(\d*), Y\+(\d*)\nButton B: X\+(\d*), Y\+(\d*)\nPrize: X=(\d*), Y=(\d*)/g)]
+    .map(machine => machine.slice(1, 7).map(Number));
 
 let sum = 0;
 
-for (let [_, Ax, Ay, Bx, By, Px, Py] of match) {
+for (let [Ax, Ay, Bx, By, Px, Py] of match) {
+
+    Px += 10000000000000;
+    Py += 10000000000000;
+
+    console.log([Ax, Ay, Bx, By, Px, Py].join('\t'));
 
     let q = Ax * By - Ay * Bx;
     let d1 = Px * By - Py * Bx;
