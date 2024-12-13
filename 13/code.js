4b13247af0829d8f8d0ebaa72ebb7379038c3125
bobmadereren <williamspurkeland@hotmail.com>
2024-12-13 14:15:13 +0100

    -console log

--- 13/code.js
+++ 13/code.js
@@ -14,8 +14,6 @@ for (let [Ax, Ay, Bx, By, Px, Py] of match) {
     Px += 10000000000000;
     Py += 10000000000000;
 
-    console.log([Ax, Ay, Bx, By, Px, Py].join('\t'));
-
     let q = Ax * By - Ay * Bx;
     let d1 = Px * By - Py * Bx;
     let d2 = -Px * Ay + Py * Ax;
