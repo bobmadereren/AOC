cfa71da7955ff265569ab6d7bf19cce97417d686
bobmadereren <williamspurkeland@hotmail.com>
2024-12-16 18:42:45 +0100

    16-2

--- 16/code.js
+++ 16/code.js
@@ -21,24 +21,39 @@ function find(c) {
 let [startY, startX] = find('S');
 let [endY, endX] = find('E');
 
-// Part 1
-let dist = grid.map(row => row.map(c => ({})));
-let queue = new MinPriorityQueue(([y, x, dy, dx, d]) => d);
+function distance(sourceY, sourceX, ...sourceDirections) {
+    let dist = grid.map(row => row.map(c => ({})));
+    let queue = new MinPriorityQueue(([y, x, dy, dx, d]) => d);
 
-queue.enqueue([startY, startX, 0, 1, 0]);
+    for (let [dy, dx] of sourceDirections)
+        queue.enqueue([sourceY, sourceX, dy, dx, 0]);
 
-while (!queue.isEmpty()) {
-    let [y, x, dy, dx, d] = queue.pop();
+    while (!queue.isEmpty()) {
+        let [y, x, dy, dx, d] = queue.pop();
 
-    if (dist[y][x][[dy, dx]] || grid[y][x] == '#') continue;
-    dist[y][x][[dy, dx]] = d;
+        if (dist[y][x][[dy, dx]] != null || grid[y][x] == '#') continue;
+        dist[y][x][[dy, dx]] = d;
 
-    if (y == endY && x == endX) break;
+        queue.enqueue([y + dy, x + dx, dy, dx, d + 1]); // Forward
+        queue.enqueue([y, x, -dx, dy, d + 1000]); // Rotate
+        queue.enqueue([y, x, dx, -dy, d + 1000]); // Rotate
+    }
 
-    queue.enqueue([y + dy, x + dx, dy, dx, d + 1]); // Forward
-    queue.enqueue([y, x, -dx, dy, d + 1000]); // Rotate
-    queue.enqueue([y, x, dx, -dy, d + 1000]); // Rotate
+    return dist;
 }
 
-console.log(dist[endY][endX]);
+// Part 1
+let endDistance = distance(endY, endX, [1, 0], [-1, 0], [0, 1], [0, -1]);
+let totalDistance = endDistance[startY][startX][[0, -1]];
 
+console.log(totalDistance);
+
+// Part 2
+let startDistance = distance(startY, startX, [0, 1]);
+let count = 0;
+for (let y in grid)
+    for (let x in grid[y])
+        if (grid[y][x] != '#')
+                count += [[1, 0], [-1, 0], [0, 1], [0, -1]].some(([dy, dx]) => startDistance[y][x][[dy, dx]] + endDistance[y][x][[-dy, -dx]] == totalDistance);
+
+console.log(count);
