dac6b154082afa56a41a18182c0756b057d8c029
bobmadereren <williamspurkeland@hotmail.com>
2024-12-18 02:14:11 +0100

    17-2

--- 17/code.js
+++ 17/code.js
@@ -1,38 +1,58 @@
-import fs from 'fs';
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
+let program = [2n, 4n, 1n, 5n, 7n, 5n, 4n, 5n, 0n, 3n, 1n, 6n, 5n, 5n, 3n, 0n];
 
-let input = fs.readFileSync(join(dirname(fileURLToPath(import.meta.url)), 'input.txt')).toString();
+function compute(A) {
 
-let [[, A, B, C, D]] = input.matchAll(/Register A: (\d+)\nRegister B: (\d+)\nRegister C: (\d+)\n\nProgram: ((\d,?)+)/g);
-let data = D.split(',').map(Number);
+    let [B, C] = [0n, 0n];
 
-let out = [];
-let i = 0;
+    let out = [];
+    let i = 0n;
 
-let combo = (operand) => {
-    switch (operand) {
-        case 0: case 1: case 2: case 3: return operand;
-        case 4: return A;
-        case 5: return B;
-        case 6: return C;
+    let combo = (operand) => {
+        switch (operand) {
+            case 0n: case 1n: case 2n: case 3n: return operand;
+            case 4n: return A;
+            case 5n: return B;
+            case 6n: return C;
+        }
     }
+
+    let execute = [
+        operand => A = A >> combo(operand),
+        operand => B = B ^ operand,
+        operand => B = combo(operand) % 8n,
+        operand => i = A == 0n ? i : operand - 2n,
+        operand => B = B ^ C,
+        operand => out.push(combo(operand) % 8n),
+        operand => B = A >> combo(operand),
+        operand => C = A >> combo(operand),
+    ];
+
+    while (i < program.length) {
+        let [opcode, operand] = [program[i], program[i + 1n]];
+        execute[opcode](operand);
+        i += 2n;
+    }
+    
+    return out;
 }
 
-let execute = [
-    operand => A = A >> combo(operand),         // 0
-    operand => B = B ^ operand,                 // 1
-    operand => B = combo(operand) % 8,          // 2
-    operand => i = A == 0 ? i : operand - 2,    // 3
-    operand => B = B ^ C,                       // 4
-    operand => out.push(combo(operand) % 8),    // 5
-    operand => B = A >> combo(operand),         // 6
-    operand => C = A >> combo(operand),         // 7
-];
+let viruses = [0n];
 
-while (i < data.length) {
-    execute[data[i]](data[i + 1]);
-    i += 2;
+let period = 3n;    // <--- number of bits reduced for the input at each recursive call of the program
+
+while(viruses.length > 0) {
+    let newViruses = [];
+    for (let aLeft of viruses) {
+        for (let aRight of Array.from({length: Number(1n << period)}, (_, i) => BigInt(i))) {
+            let A = (aLeft << period) + aRight;
+            let out = compute(A);
+            if (program.join().match(out.join() + '$'))
+                newViruses.push(A);
+        }
+    }
+    viruses = newViruses;
+    let [virus] = viruses;
+    if(compute(virus).length == program.length) break;
 }
 
-console.log(out.join());
+console.log(viruses[0]);
