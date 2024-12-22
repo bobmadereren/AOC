709cd39dbab6d1b65dde4e3d4f476b0c1a27ecb2
bobmadereren <williamspurkeland@hotmail.com>
2024-12-22 03:29:12 +0100

    20-2

--- 20/code.js
+++ 20/code.js
@@ -18,7 +18,6 @@ function find(c) {
     }
     return [y, x];
 }
-let [startY, startX] = find('S');
 let [endY, endX] = find('E');
 
 let dist = grid.map(row => row.map(c => null));
@@ -32,22 +31,23 @@ for (let [y, x, d] of queue) {
 }
 queue = null;
 
-let saved = grid.map(row => row.map(c => ({})));
-
+let cheatLength = 20; // 2 for Part 1
 let count = 0;
 
 for (let y = 0; y < size; y++)
     for (let x = 0; x < size; x++)
-        for (let [dy, dx] of [[-2, 0], [-1, 1], [0, 2], [1, 1], [2, 0], [1, -1], [0, -2], [-1, -1]])
-            if (
-                0 <= x + dx &&
-                0 <= y + dy &&
-                y + dy < size &&
-                x + dx < size &&
-                grid[y][x] != '#' &&
-                grid[y + dy][x + dx] != '#'
-            )
-                if (dist[y + dy][x + dx] + 2 <= dist[y][x] - 100)
-                    count++;
+        for (let dy = -cheatLength; dy <= cheatLength; dy++)
+            for (let dx = - cheatLength; dx <= cheatLength; dx++)
+                if (Math.abs(dy) + Math.abs(dx) <= cheatLength)
+                    if (
+                        0 <= x + dx &&
+                        0 <= y + dy &&
+                        y + dy < size &&
+                        x + dx < size &&
+                        grid[y][x] != '#' &&
+                        grid[y + dy][x + dx] != '#'
+                    )
+                        if (dist[y + dy][x + dx] + Math.abs(dy) + Math.abs(dx) <= dist[y][x] - 100)
+                            count++;
 
 console.log(count);
