2b14db8fadd90b426bf5e442997b2f7e2505eca6
bobmadereren <williamspurkeland@hotmail.com>
2024-12-22 03:15:33 +0100

    20-1

---
+++ 20/code.js
@@ -0,0 +1,53 @@
+import fs from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+let input = fs.readFileSync(join(dirname(fileURLToPath(import.meta.url)), 'input.txt')).toString();
+
+let grid = input.split(/\n/).filter(line => line).map(row => row.split(''));
+let size = grid.length;
+
+function find(c) {
+    let [y, x] = [0, 0];
+    while (grid[y][x] != c) {
+        x++;
+        if (x == grid[y].length) {
+            x = 0;
+            y++;
+        }
+    }
+    return [y, x];
+}
+let [startY, startX] = find('S');
+let [endY, endX] = find('E');
+
+let dist = grid.map(row => row.map(c => null));
+
+let queue = [[endY, endX, 0]];
+for (let [y, x, d] of queue) {
+    if (dist[y][x] != null || grid[y][x] == '#') continue;
+    dist[y][x] = d;
+    for (let [dy, dx] of [[1, 0], [-1, 0], [0, 1], [0, -1]])
+        queue.push([y + dy, x + dx, d + 1]);
+}
+queue = null;
+
+let saved = grid.map(row => row.map(c => ({})));
+
+let count = 0;
+
+for (let y = 0; y < size; y++)
+    for (let x = 0; x < size; x++)
+        for (let [dy, dx] of [[-2, 0], [-1, 1], [0, 2], [1, 1], [2, 0], [1, -1], [0, -2], [-1, -1]])
+            if (
+                0 <= x + dx &&
+                0 <= y + dy &&
+                y + dy < size &&
+                x + dx < size &&
+                grid[y][x] != '#' &&
+                grid[y + dy][x + dx] != '#'
+            )
+                if (dist[y + dy][x + dx] + 2 <= dist[y][x] - 100)
+                    count++;
+
+console.log(count);
